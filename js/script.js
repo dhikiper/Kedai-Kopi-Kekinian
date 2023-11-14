6f44325145a529a2cec2a3b2d80e187ade49a9c3
Dhiki Pernando <fernandodhiki@gmail.com>
2023-11-14 14:15:36 +0700

    merevisi untuk menambah beberapa fungsi website seperti menambah shoping cart dan menjalankan fungsi produk

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
-// Toggle class active
+// Toggle class active menu
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika menu di klik
-document.querySelector("#menu").onclick = () => {
+document.querySelector("#menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik di luar sidebar unutk menghilangkan nav
-const menu = document.querySelector("#menu");
+// Toggle class active search-form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+// Ketika search-form di klik
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
+// Toggle class active shopping-cart
+const shoppingCart = document.querySelector(".shopping-cart");
+// Ketika shopping-cart di klik
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar element
+const menu = document.querySelector("#menu");
+const SB = document.querySelector("#search-btn");
+const SC = document.querySelector("#shopping-cart-btn");
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!SB.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!SC.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik Tombol Close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
